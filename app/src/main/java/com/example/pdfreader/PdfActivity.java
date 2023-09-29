150b7c23ef23110ad2ec16bb3a87ad085390ede9
Ashok Pahal <99093716+AshokPahal@users.noreply.github.com>
2023-09-29 19:33:20 +0530

    PdfViewer

---
+++ app/src/main/java/com/example/pdfreader/PdfActivity.java
@@ -0,0 +1,31 @@
+package com.example.pdfreader;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.File;
+
+public class PdfActivity extends AppCompatActivity {
+
+    String filePath = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf);
+
+        PDFView pdfView = findViewById(R.id.pdfView);
+
+        filePath = getIntent().getStringExtra("path");
+
+        assert filePath != null;
+        File file = new File(filePath);
+        Uri path = Uri.fromFile(file);
+        pdfView.fromUri(path).load();
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+    }
+}
+
+rootProject.name = "PdfReader"
+include(":app")
